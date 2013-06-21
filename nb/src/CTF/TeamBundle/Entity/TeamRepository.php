839bfcb8c017e63863c94e48ecc6b5f9bfa68963
Amrith Nayak <amrith92@gmail.com>
2013-06-22 01:39:18 +0530

    Reduced size of login-bg image

--- nb/src/CTF/TeamBundle/Entity/TeamRepository.php
+++ nb/src/CTF/TeamBundle/Entity/TeamRepository.php
@@ -61,4 +61,23 @@ class TeamRepository extends EntityRepository {
         
         return $ret;
     }
+    
+    public function getGlobalRanks() {
+        $em = $this->getEntityManager();
+        $connection = $em->getConnection();
+        $connection->executeQuery('SET @rownum := 0');
+        $statement = $connection->executeQuery("SELECT rank, score, name, status FROM (SELECT @rownum := @rownum + 1 AS rank, score, name, status FROM team ORDER BY score DESC) as result WHERE 1");
+        $ret = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return $ret;
+    }
+    
+    public function getSurrounding($score) {
+        $em = $this->getEntityManager();
+        $connection = $em->getConnection();
+        $statement = $connection->executeQuery("SELECT name, `status`, score FROM (SELECT t.*, @n1:=@n1 + 1 num, @n2:=IF(score = " . $score . ", @n1, @n2) pos FROM team t, (SELECT @n1:=0, @n2:=0) n ORDER BY t.id) q WHERE @n2 >= num - 2 AND @n2 <= num + 2;");
+        $ret = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return $ret;
+    }
 }
