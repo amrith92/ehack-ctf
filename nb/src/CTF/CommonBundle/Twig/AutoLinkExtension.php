b5d62a8d563aee4d250b98214e6b14341cb4eb33
Amrith Nayak <amrith92@gmail.com>
2013-06-30 00:21:17 +0530

    Fixed http/https bug in AutoLinkExtension

--- nb/src/CTF/CommonBundle/Twig/AutoLinkExtension.php
+++ nb/src/CTF/CommonBundle/Twig/AutoLinkExtension.php
@@ -15,14 +15,13 @@ class AutoLinkExtension extends \Twig_Extension {
     }
     
     static public function autolink($string) {
-        $regexp = "/(<a.*?>)?(https?)?(:\/\/)?(\w+\.)?(\w+)\.(\w+[\/\d\w\-\_\#\@\%\&\!]*)(<\/a.*?>)?/i";
+        $regexp = "/(<a.*?>)?(http|https)(:\/\/)?(\w+\.)?(\w+)\.(\w+[\/\d\w\-\_\#\@\%\&\!]*)(<\/a.*?>)?/i";
         $anchorMarkup = '<a href="%s" target="_blank" >%s</a>';
 
         preg_match_all($regexp, $string, $matches, \PREG_SET_ORDER);
 
         foreach ($matches as $match) {
             if (empty($match[1]) && empty($match[7])) {
-                //$http = $match[2] ? $match[2] : 'http';
                 $replace = sprintf($anchorMarkup, $match[0], $match[0]);
                 $string = str_replace($match[0], $replace, $string);
             }
