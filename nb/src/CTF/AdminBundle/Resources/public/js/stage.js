f9c7793f9f012f7ad6d94bf778d297ab88206859
Amrith Nayak <amrith92@gmail.com>
2013-06-30 00:12:47 +0530

    Upgraded project to leverage assetic

---
+++ nb/src/CTF/AdminBundle/Resources/public/js/stage.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+    $('#stage-list-container').on('click', '#add-more-stages', function(e) {
+        e.preventDefault();
+        
+        $.get(Routing.generate('ctf_admin_stage_form', null), null, function(data) {
+            $('#stage-form-dyn').empty();
+            $('#stage-form-dyn').html(data).fadeIn();
+        });
+    });
+    
+    $('#stage-list-container').on('click', 'a.list-item', function(e) {
+        e.preventDefault();
+        
+        $.get($(this).attr('href'), null, function(data) {
+            $('#stage-form-dyn').empty();
+            $('#stage-form-dyn').html(data).fadeIn();
+        });
+    });
+    
+    $('#stage-form-container').on('submit', '#stage-form', function(e) {
+        e.preventDefault();
+        
+        $.post($('#stage-form').attr('action'), $('#stage-form').serialize(), function(data) {
+            if ('true' == data) {
+                $('#stage-form').fadeOut();
+                $('#stage-form').fadeIn('fast', function() {
+                    $('#ctf_questbundle_stagetype_name').val('');
+                    $('#ctf_questbundle_stagetype_description').val('');
+                });
+                
+                $('#stage-form-status').html('<div class="alert alert-success">Successfully Saved!</div>').show().fadeOut(2200);
+                
+                $.get(Routing.generate('ctf_admin_stage_list', null), null, function(data) {
+                    $('#stage-list-dyn').html(data).show();
+                });
+                $.get(Routing.generate('ctf_admin_stage_list', { 'q': 1 }), null, function(data) {
+                    $('#qlist-dyn').html(data).show();
+                });
+            } else {
+                $('#stage-form-status').html('<div class="alert alert-error">Could not save!</div>').show().fadeOut(1200);
+            }
+        });
+    });
+    
+    $('#qlist-container').on('click', 'a.list-item', function(e) {
+        e.preventDefault();
+        
+        $.get($(this).attr('href'), null, function(data) {
+            $('#question-form-dyn').empty();
+            $('#question-form-dyn').html(data).fadeIn();
+            tinyMCE.init({
+                selector: ".rich",
+                mode : "specific_textareas"
+            });
+        });
+    });
+    
+    $('#qlist-container').on('click', '#add-more-questions', function(e) {
+        e.preventDefault();
+        
+        $.get(Routing.generate('ctf_admin_stage_question', null), null, function(data) {
+            $('#question-form-dyn').empty();
+            $('#question-form-dyn').html(data).show();
+            tinyMCE.init({
+                selector: ".rich",
+                mode : "specific_textareas"
+            });
+            $('#qtabs').tab();
+        });
+    });
+});
