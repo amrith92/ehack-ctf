4101c5e297d2931ad738e75346aa3579b79fedfd
Amrith Nayak <amrith92@gmail.com>
2013-07-12 22:53:25 +0530

    Fixes for google-maps. Fix for issue #55

--- nb/src/CTF/UserBundle/Resources/public/js/editprofile.js
+++ nb/src/CTF/UserBundle/Resources/public/js/editprofile.js
@@ -1,33 +1,25 @@
 var map, marker = null;
 function initialiseMap() {
+    var latlng = new google.maps.LatLng(13.0810, 80.2740);
     var myOptions = {
-        center: new google.maps.LatLng(13.0810, 80.2740),
+        center: latlng,
         zoom: 6,
-        mapTypeControl: false,
-        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-        navigationControl: false,
-        navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     marker = new google.maps.Marker({
-        position: map.getCenter(),
+        position: latlng,
         map: map,
         title:"You are here",
         draggable: true
     });
-    map.panTo(marker.getPosition());
-    geocodePosition(marker.getPosition());
+    map.panTo(latlng);
+    geocodePosition(latlng);
     google.maps.event.addListener(marker, 'dragend', function(evt) {
         $("#ctf_userbundle_usertype_location").val(evt.latLng.lat() + "," + evt.latLng.lng());
         geocodePosition(marker.getPosition());
     });
-}
-
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(-33.8688, 151.2195),
-    };
+    
     var input = document.getElementById('ctf_userbundle_usertype_org');
     var options = {
         types: ['establishment']
@@ -37,7 +29,11 @@ function initialize() {
 
 function onLocateClick() {
     if(geoPosition.init()) {
-        geoPosition.getCurrentPosition(showPosition,function(){ alert('Could not find you! :('); },{enableHighAccuracy:true});
+        geoPosition.getCurrentPosition(showPosition, function(e) {
+            alert(e.message);
+        }, {
+            enableHighAccuracy: true
+        });
     }
 }
 
@@ -45,7 +41,7 @@ function showPosition(p) {
     var latitude = parseFloat( p.coords.latitude );
     var longitude = parseFloat( p.coords.longitude );
     $('#ctf_userbundle_usertype_location').val(latitude + "," + longitude);
-    var pos=new google.maps.LatLng( latitude , longitude);
+    var pos = new google.maps.LatLng( latitude , longitude);
     map.setCenter(pos);
     map.setZoom(14);
 
@@ -136,24 +132,27 @@ $(document).ready(function() {
                 changeMonth: true,
                 changeYear: true
             });
-            initialize();
             initialiseMap();
             var mapval = $('#ctf_userbundle_usertype_location').val();
             if (null !== mapval) {
                 var latlng = mapval.split(',');
-                showPosition({ coords: { latitude: latlng[0], longitude: latlng[1] } });
+                if (latlng.length > 1) {
+                    showPosition({ coords: { latitude: latlng[0], longitude: latlng[1] } });
+                    geocodePosition(marker.getPosition());
+                }
             }
-            $('#regTabs a[href="#address"]').tab('show');
+            $('a[href="#address"]').click();
         }, "html");
     });
     
-    initialize();
     initialiseMap();
     
     var mapval = $('#ctf_userbundle_usertype_location').val();
     if (null != mapval && 'NaN,NaN' != mapval) {
         var latlng = mapval.split(',');
-        showPosition({ coords: { latitude: latlng[0], longitude: latlng[1] } });
-        geocodePosition(marker.getPosition());
+        if (latlng.length > 1) {
+            showPosition({ coords: { latitude: latlng[0], longitude: latlng[1] } });
+            geocodePosition(marker.getPosition());
+        }
     }
 });
